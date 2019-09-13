d75289e38d6d4bd352be792a6fcf4986b7d53da9
SKP <skp.frl@gmail.com>
2019-09-13 19:29:25 +0530

    :lipstick: Add mainTheme

---
+++ src/themes/mainTheme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from 'styled-components';
+
+export const mainTheme: DefaultTheme = {
+	colors: {
+		primary: '#2ac46a',
+		primaryDark: '#00A896',
+		secondary: '#fafbed',
+		tertiary: '#028090',
+		tertiaryDark: '#05668D',
+		grey: '#333',
+		lightGrey: '#f7f7f7',
+		mediumGrey: '#eee'
+	}
+};
