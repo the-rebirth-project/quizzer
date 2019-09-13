e9fd325b4ee4fa91180dc13f1860f5725883941b
SKP <skp.frl@gmail.com>
2019-09-13 19:26:33 +0530

    :truck: Move Question interface to separate file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 // CENTRAL EXPORT POINT
 export * from './actions'; // exports type string constants for action creators
 export * from './rootTypes'; // exports types for our RootState and RootAction and the type for Store
+export * from './Question.interface';
