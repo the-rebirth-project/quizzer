9a80b2a095781a72c04b65caf0a1c9a559d7f3e4
SKP <skp.frl@gmail.com>
2019-09-19 20:35:01 +0530

    :bug: Fix forward sorting

--- src/helpers/arrayMove.ts
+++ src/helpers/arrayMove.ts
@@ -2,30 +2,24 @@
 
 // Not really performant but it serves our use case since the array will be less than 101 elements
 export const arrayMove = (oldI: number, newI: number, array: any[]): any[] => {
-	const forward = newI > oldI;
 	const backward = oldI > newI;
+	const movedElem = array[oldI];
 
-	// forward sorting is broken. pls fix (elem to be sorted for eg does not appear at last index like it shld)
-	if (forward) {
-		const sortedArray = [
-			...array.slice(0, oldI),
-			...array.slice(oldI + 1, newI),
-			array[oldI],
-			...array.slice(newI)
-		];
-		return sortedArray;
-	} else if (backward) {
+	if (backward) {
 		const sortedArray = [
 			...array.slice(0, newI),
-			array[oldI],
+			movedElem,
 			...array.slice(newI, oldI),
 			...array.slice(oldI + 1)
 		];
 		return sortedArray;
+	} else {
+		const sortedArray = [
+			...array.slice(0, oldI),
+			...array.slice(oldI + 1, newI + 1),
+			movedElem,
+			...array.slice(newI + 1)
+		];
+		return sortedArray;
 	}
-	// by default, return the initial array
-	return array;
 };
-// [2, 3, 4, 5, 1]
-// [1, 2, 3, 4, 5]
-// [1, 3, 2, 4, 5]
