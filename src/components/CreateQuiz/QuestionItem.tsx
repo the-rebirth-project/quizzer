ef5915103b0af3679151c18c87d13be80b6b131b
SKP <skp.frl@gmail.com>
2019-09-20 19:10:02 +0530

    :sparkles: Implement Modal

--- src/components/CreateQuiz/QuestionItem.tsx
+++ src/components/CreateQuiz/QuestionItem.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import { Modal } from '../Modal';
 import { SortableElement } from 'react-sortable-hoc';
+import { Root } from './questionItemStyles';
 
 const WrappedComponent: React.FC<{ type: string }> = ({ type }) => {
+	const [modalOpen, setModalOpen] = useState(false);
+	const memoizedOpen = useMemo(() => modalOpen, [modalOpen]);
+
+	const setOpen = (): void => {
+		setModalOpen(!modalOpen);
+	};
+
 	return (
-		<div>
+		<Root>
 			{type === 'random' && <h3>Random</h3>}
 			{type === 'custom' && <h3>Custom</h3>}
-		</div>
+			<button onClick={setOpen}>Click me to Open Modal!</button>
+			<Modal
+				aria-labelledby="modal-label"
+				aria-describedby="model-desc"
+				modalOpen={memoizedOpen}
+				setOpen={setOpen}
+			>
+				<h1 id="modal-label">I'm a Modal!</h1>
+				<p id="modal-desc">I'm a non-animated Modal!</p>
+			</Modal>
+		</Root>
 	);
 };
 
