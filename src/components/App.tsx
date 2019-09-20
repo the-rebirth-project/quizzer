c1d0e05f19914dc3860b2bc46788ab70e78feab8
SKP <skp.frl@gmail.com>
2019-09-20 19:06:54 +0530

    :lipstick: Add css variable for rgb variant of secondary color

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,9 +7,8 @@ import { RootState } from '../types';
 
 const Background = styled(animated.div)`
 	--primary-rgb-color: 38, 188, 99; /* rgb version of our primary green color */
+	--secondary-rgb-color: 242, 243, 229;
 	height: 100vh;
-	position: relative;
-	z-index: 1000;
 `;
 
 export const App: React.FC = () => {
