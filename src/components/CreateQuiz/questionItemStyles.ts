f7fa213ce7b1b72f8fb446628fe8860f0d9d33b2
SKP <skp.frl@gmail.com>
2019-09-20 19:10:28 +0530

    :lipstick: Style QuestionItem

---
+++ src/components/CreateQuiz/questionItemStyles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Root = styled.div`
+	padding: 0.5rem;
+	border-radius: 15px;
+	background-color: ${props => props.theme.colors.secondary};
+	color: ${props => props.theme.colors.grey};
+	box-shadow: 2px 3.5px 6px rgba(var(--secondary-rgb-color), 0.25);
+	position: relative;
+	z-index: 15;
+	margin: 2rem 0rem;
+	font-size: 3rem;
+	font-weight: 300;
+`;
