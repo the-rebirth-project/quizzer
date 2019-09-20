2c707eef912e69040e89c78a619633ff08d81e7a
SKP <skp.frl@gmail.com>
2019-09-20 23:01:05 +0530

    :sparkles: Implement create question Modal

--- src/components/CreateQuiz/QuestionItem.tsx
+++ src/components/CreateQuiz/QuestionItem.tsx
@@ -1,27 +1,23 @@
-import React, { useMemo, useState } from 'react';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { openModal } from '../../actions';
 import { Modal } from '../Modal';
 import { SortableElement } from 'react-sortable-hoc';
 import { Root } from './questionItemStyles';
 
 const WrappedComponent: React.FC<{ type: string }> = ({ type }) => {
-	const [modalOpen, setModalOpen] = useState(false);
-	const memoizedOpen = useMemo(() => modalOpen, [modalOpen]);
+	const dispatch = useDispatch();
 
-	const setOpen = (): void => {
-		setModalOpen(!modalOpen);
+	const onButtonClick = (): void => {
+		dispatch(openModal());
 	};
 
 	return (
 		<Root>
 			{type === 'random' && <h3>Random</h3>}
 			{type === 'custom' && <h3>Custom</h3>}
-			<button onClick={setOpen}>Click me to Open Modal!</button>
-			<Modal
-				aria-labelledby="modal-label"
-				aria-describedby="model-desc"
-				modalOpen={memoizedOpen}
-				setOpen={setOpen}
-			>
+			<button onClick={onButtonClick}>Click me to Open Modal!</button>
+			<Modal aria-labelledby="modal-label" aria-describedby="model-desc">
 				<h1 id="modal-label">I'm a Modal!</h1>
 				<p id="modal-desc">I'm a non-animated Modal!</p>
 			</Modal>
