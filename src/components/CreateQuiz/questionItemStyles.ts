7f4f554b907e3541b3f2911cc9a768e5d6166b3d
SKP <skp.frl@gmail.com>
2019-09-20 23:02:55 +0530

    :lipstick: Style skeleton of QuestionItem

--- src/components/CreateQuiz/questionItemStyles.ts
+++ src/components/CreateQuiz/questionItemStyles.ts
@@ -5,9 +5,9 @@ export const Root = styled.div`
 	border-radius: 15px;
 	background-color: ${props => props.theme.colors.secondary};
 	color: ${props => props.theme.colors.grey};
-	box-shadow: 2px 3.5px 6px rgba(var(--secondary-rgb-color), 0.25);
+	box-shadow: 2px 3.5px 6px rgba(var(--secondary-rgb-color), 0.15);
 	position: relative;
-	z-index: 15;
+	z-index: 1;
 	margin: 2rem 0rem;
 	font-size: 3rem;
 	font-weight: 300;
