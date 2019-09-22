e5b18961014e23b309dee1f0244e5ab4a65f11d1
SKP <skp.frl@gmail.com>
2019-09-22 13:09:50 +0530

    :truck: Rename file

---
+++ src/reducers/quizReducers.ts
@@ -0,0 +1,26 @@
+import { createReducer } from 'typesafe-actions';
+import { fetchQuestions, startQuiz, sortQuestion } from '../actions';
+import { Question } from '../types';
+interface IState {
+	readonly questions: Question[];
+	readonly started: boolean;
+}
+
+const initialState: IState = {
+	questions: [],
+	started: false
+};
+
+export const quizReducer = createReducer(initialState)
+	.handleAction(fetchQuestions, (state, action) => ({
+		...state,
+		questions: [...state.questions, ...action.payload]
+	}))
+	.handleAction(startQuiz, (state, _) => ({
+		...state,
+		started: true
+	}))
+	.handleAction(sortQuestion, (state, action) => ({
+		...state,
+		questions: action.payload
+	}));
