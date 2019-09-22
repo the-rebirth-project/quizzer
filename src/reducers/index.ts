83fa2fe249b21d610efbc2b1ef658a9afe7d4d99
SKP <skp.frl@gmail.com>
2019-09-22 13:14:00 +0530

    :art: Restructure store

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,18 +1,18 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
-import { menuReducer } from './menuReducers';
+import { quizReducer } from './quizReducers';
 import { questionReducer } from './questionReducers';
 import { modalReducer } from './modalReducers';
 
 export const rootReducer = (history: any) =>
 	combineReducers({
 		router: connectRouter(history),
-		menu: menuReducer,
+		quiz: quizReducer,
 		question: questionReducer,
 		modal: modalReducer
 	});
 
 // CENTRAL EXPORTS
-export * from './menuReducers';
+export * from './quizReducers';
 export * from './questionReducers';
 export * from './modalReducers';
