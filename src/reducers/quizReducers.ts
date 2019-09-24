a7f9dbd7bd135017a8e03b2a137d276e789b876f
SKP <skp.frl@gmail.com>
2019-09-24 21:29:42 +0530

    :sparkles: Handle saveEditedQuestion action

--- src/reducers/quizReducers.ts
+++ src/reducers/quizReducers.ts
@@ -1,6 +1,11 @@
 import { createReducer } from 'typesafe-actions';
 import uuid from 'uuid/v4';
-import { fetchQuestions, startQuiz, sortQuestion } from '../actions';
+import {
+	fetchQuestions,
+	startQuiz,
+	sortQuestion,
+	saveEditedQuestion
+} from '../actions';
 import { Question } from '../types';
 
 interface IState {
@@ -32,4 +37,26 @@ export const quizReducer = createReducer(initialState)
 	.handleAction(sortQuestion, (state, action) => ({
 		...state,
 		questions: action.payload
-	}));
+	}))
+	.handleAction(saveEditedQuestion, (state, action) => {
+		// filter out the question that was edited
+		const editedQuestion = state.questions.filter(
+			q => q.qId === action.payload.qId
+		)[0];
+
+		// overwrite the selected question with the new properties
+		const newQuestion = {
+			...editedQuestion,
+			...action.payload
+		};
+
+		// filter out all the questions whose id is not equal to the id of editedQuestion and finally overwrite the selected question with the newQuestion
+
+		return {
+			...state,
+			questions: [
+				...state.questions.filter(q => q.qId !== action.payload.qId),
+				newQuestion
+			]
+		};
+	});
