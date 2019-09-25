df0591005f061ff9d0c35d8f66b4cbac634ff676
SKP <skp.frl@gmail.com>
2019-09-25 18:21:56 +0530

    :recycle: Refactor modal action creators

--- src/actions/modalActions.ts
+++ src/actions/modalActions.ts
@@ -1,4 +1,7 @@
 import { createStandardAction } from 'typesafe-actions';
-import { OPEN_CREATE_MODAL } from '../types';
+import { OPEN_CREATE_MODAL, OPEN_EDIT_MODAL } from '../types';
 
-export const openModal = createStandardAction(OPEN_CREATE_MODAL)<undefined>();
+export const openCreateModal = createStandardAction(OPEN_CREATE_MODAL)<
+	undefined
+>();
+export const openEditModal = createStandardAction(OPEN_EDIT_MODAL)<undefined>();
