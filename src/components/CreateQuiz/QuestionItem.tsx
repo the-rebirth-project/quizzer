1ecabe7071855765e17cef4db25714bf0249d5ef
SKP <skp.frl@gmail.com>
2019-09-25 18:24:55 +0530

    :art: Implement new refactored Modal component

--- src/components/CreateQuiz/QuestionItem.tsx
+++ src/components/CreateQuiz/QuestionItem.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { SortableElement } from 'react-sortable-hoc';
-import { useDispatch } from 'react-redux';
 import { Modal } from '../Modal';
 import { EditForm } from './EditForm';
-import { openModal } from '../../actions';
-import { Question } from '../../types';
+import { openEditModal } from '../../actions';
+import { RootState, Question } from '../../types';
 import { Root } from './questionItemStyles';
 
 interface QuestionItemProps {
@@ -14,17 +14,26 @@ interface QuestionItemProps {
 
 const WrappedComponent: React.FC<QuestionItemProps> = ({ type, question }) => {
 	const dispatch = useDispatch();
+	const editModalOpen = useSelector(
+		(state: RootState) => state.modal.editModalOpen
+	);
 
-	const onButtonClick = (): void => {
-		dispatch(openModal());
+	const onBtnClick = (): void => {
+		// keep in mind that openEditModal and similar modal actions can work in reverse too i.e close the modal
+		dispatch(openEditModal());
 	};
 
 	return (
 		<Root>
 			{type === 'random' && <h3>Random</h3>}
 			{type === 'custom' && <h3>Custom</h3>}
-			<button onClick={onButtonClick}>Click me to Open Modal!</button>
-			<Modal aria-labelledby="modal-label" aria-describedby="model-desc">
+			<button onClick={onBtnClick}>Click me to Open Modal!</button>
+			<Modal
+				open={editModalOpen}
+				onModalClose={openEditModal}
+				aria-labelledby="modal-label"
+				aria-describedby="model-desc"
+			>
 				<EditForm question={question} />
 			</Modal>
 		</Root>
