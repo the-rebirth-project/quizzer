b86ad0187af5fa3dee94d60029ee90e5b0be062a
SKP <skp.frl@gmail.com>
2019-09-28 17:45:25 +0530

    :fire: Remove use of redux for handling modalOpen state

--- src/actions/modalActions.ts
+++
@@ -1,7 +0,0 @@
-import { createStandardAction } from 'typesafe-actions';
-import { OPEN_CREATE_MODAL, OPEN_EDIT_MODAL } from '../types';
-
-export const openCreateModal = createStandardAction(OPEN_CREATE_MODAL)<
-	undefined
->();
-export const openEditModal = createStandardAction(OPEN_EDIT_MODAL)<undefined>();
