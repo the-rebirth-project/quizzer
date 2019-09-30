ef3fbdf50691912c660612c6c9446be3aa2d280e
SKP <skp.frl@gmail.com>
2019-09-30 19:33:09 +0530

    :lipstick: Add transition for modal on mount and on unmount

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSpring } from 'react-spring';
+import { useTransition } from 'react-spring';
 import { Root, Background, Content } from './styles';
 
 interface ModalProps {
@@ -16,20 +16,26 @@ export const Modal: React.FC<ModalProps> = ({
 		setModalOpen(false);
 	};
 
-	const animProps = useSpring({
+	const transitions = useTransition(open, null, {
 		from: { opacity: 0, transform: 'scale(0)' },
-		to: { opacity: 1, transform: 'scale(1)' },
+		enter: { opacity: 1, transform: 'scale(1)' },
+		leave: { opacity: 0, transform: 'scale(0)' },
 		config: { tension: 80, friction: 10 }
 	});
 
 	return (
-		<div>
-			{open && (
-				<Root open={open}>
-					<Background onClick={onClickOutside} />
-					<Content style={animProps}>{children}</Content>
-				</Root>
+		<>
+			{transitions.map(
+				({ item, key, props }) =>
+					item && (
+						<Root>
+							<Background onClick={onClickOutside} />
+							<Content key={key} style={props}>
+								{children}
+							</Content>
+						</Root>
+					)
 			)}
-		</div>
+		</>
 	);
 };
