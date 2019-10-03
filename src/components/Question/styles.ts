9ec1ee077e46e32c390c459afb6cb1cdee548722
SKP <skp.frl@gmail.com>
2019-10-03 22:05:03 +0530

    :lipstick: Style timer

--- src/components/Question/styles.ts
+++ src/components/Question/styles.ts
@@ -85,18 +85,39 @@ export const Qnum = styled.span`
 	font-weight: 700;
 `;
 
-export const Timer = styled.div<TimerProps>`
-	opacity: ${props => (props.completed ? 0 : 1)};
+export const Timer = styled.div`
 	position: absolute;
 	top: 50%;
 	left: 50%;
 	transform: translate(-50%, -50%);
 	z-index: 20;
-	border-radius: 10px;
+	border-radius: 50%;
 	background-color: ${props => props.theme.colors.grey};
 	color: ${props => props.theme.colors.secondary};
-	padding: 1rem;
-	font-size: 1.1rem;
+	padding: 0.8rem;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 6rem;
+	height: 6rem;
+	font-size: 1.7rem;
 `;
 
 Timer.displayName = 'Timer';
+
+export const TimeoutOverlay = styled.div<TimerProps>`
+	height: 100vh;
+	width: 100vw;
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	z-index: 100;
+	background-color: #ff0000;
+	transform: ${props => (props.completed ? 'scale(1)' : 'scale(0)')};
+	transition: transform 0.5s;
+	font-size: 12rem;
+	text-transform: uppercase;
+	letter-spacing: 5px;
+	color: ${props => props.theme.colors.secondary};
+`;
