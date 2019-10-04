474b90b6755f92cecde435373e1f067ea79b6d7b
SKP <skp.frl@gmail.com>
2019-10-04 20:45:07 +0530

    :sparkles: Add redux state management for modal open

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,13 +2,12 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import { quizReducer } from './quizReducers';
 import { questionReducer } from './questionReducers';
+import { modalReducer } from './modalReducers';
+
 export const rootReducer = (history: any) =>
 	combineReducers({
 		router: connectRouter(history),
 		quiz: quizReducer,
-		question: questionReducer
+		question: questionReducer,
+		modal: modalReducer
 	});
-
-// CENTRAL EXPORTS
-export * from './quizReducers';
-export * from './questionReducers';
