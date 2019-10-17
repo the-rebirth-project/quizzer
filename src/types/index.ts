ff07f3e7404dd067ced20394ec74c5b2c34a8aa9
SKP <skp.frl@gmail.com>
2019-10-17 18:13:07 +0530

    :art: Add exports for Team.interface.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@
 export * from './actions'; // exports type string constants for action creators
 export * from './rootTypes'; // exports types for our RootState and RootAction and the type for Store
 export * from './Question.interface';
+export * from './Team.interface';
