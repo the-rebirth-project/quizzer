f9df748e57d562d8811b9f345031676c8cf21ba3
SKP <skp.frl@gmail.com>
2019-10-19 17:05:20 +0530

    :recycle: Refactor handling of tool toggle state

--- src/reducers/toolbarReducers.ts
+++ src/reducers/toolbarReducers.ts
@@ -2,6 +2,7 @@ import { createReducer } from 'typesafe-actions';
 import { editMode, deleteMode } from '../actions';
 
 interface IState {
+	[index: string]: any;
 	readonly editModeState: boolean;
 	readonly deleteModeState: boolean;
 }
@@ -11,13 +12,28 @@ const initialState: IState = {
 	deleteModeState: false
 };
 
+// should be called each time an action for enabling a tool is dispatched.
+// this function disables every tool which is enabled i.e set to true
+const disableOtherTools = (state: IState): void => {
+	// eslint-disable-next-line
+	Object.keys(state).map(key => {
+		if (state[key] === true) state[key] = false;
+	});
+};
+
 // code may get messy as the toolbar scales to more tools. might want to find a better solution for this one
 export const toolbarReducer = createReducer(initialState)
-	.handleAction(editMode, (state, action) => ({
-		deleteModeState: action.payload === 'ON' ? false : state.deleteModeState,
-		editModeState: action.payload === 'ON' ? true : false
-	}))
-	.handleAction(deleteMode, (state, action) => ({
-		editModeState: action.payload === 'ON' ? false : state.editModeState,
-		deleteModeState: action.payload === 'ON' ? true : false
-	}));
+	.handleAction(editMode, (state, action) => {
+		disableOtherTools(state);
+		return {
+			...state,
+			editModeState: action.payload === 'ON' ? true : false
+		};
+	})
+	.handleAction(deleteMode, (state, action) => {
+		disableOtherTools(state);
+		return {
+			...state,
+			deleteModeState: action.payload === 'ON' ? true : false
+		};
+	});
