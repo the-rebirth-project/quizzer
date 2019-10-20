77c2a58ba013dcb465b2d06fc0739b33e43e0484
SKP <skp.frl@gmail.com>
2019-10-20 20:00:46 +0530

    :sparkles: Add multiplayer based scoring system

--- src/components/Question/Scoreboard.tsx
+++ src/components/Question/Scoreboard.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../types';
-import { Root } from './scoreboardStyles';
+import {
+	Root,
+	ScoreboardTitle,
+	LeftContainer,
+	RightContainer,
+	Score
+} from './scoreboardStyles';
 
 /** RULES
  * Provide props to component describing how many teams are participating
@@ -11,16 +17,50 @@ import { Root } from './scoreboardStyles';
 
 export const Scoreboard: React.FC = () => {
 	const teams = useSelector((state: RootState) => state.scoreboard.teams);
+	const firstTeam = teams.reduce((prevTeam, team) => {
+		return prevTeam.score > team.score ? prevTeam : team;
+	});
+	const lastTeam = teams.reduce((prevTeam, team) => {
+		return prevTeam.score < team.score ? prevTeam : team;
+	});
+	const sortedTeams = [
+		firstTeam,
+		...teams.filter(t => t !== firstTeam && t !== lastTeam),
+		lastTeam
+	];
 
 	return (
 		<Root>
-			<h1>Scoreboard</h1>
+			<LeftContainer>
+				<ScoreboardTitle>Scoreboard</ScoreboardTitle>
+			</LeftContainer>
 
-			{teams.map(t => (
-				<h3>
-					{t.tName}: {t.score}
-				</h3>
-			))}
+			<RightContainer>
+				{sortedTeams.map((t, i) => {
+					// if index is 0
+					if (i === 0) {
+						return (
+							<Score first>
+								{t.tName}: {t.score}
+							</Score>
+						);
+					} else if (i === sortedTeams.length - 1) {
+						// if last index
+						return (
+							<Score last>
+								{t.tName}: {t.score}
+							</Score>
+						);
+					} else {
+						// if any indices between first and last
+						return (
+							<Score>
+								{t.tName}: {t.score}
+							</Score>
+						);
+					}
+				})}
+			</RightContainer>
 		</Root>
 	);
 };
