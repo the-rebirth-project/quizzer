49cf34c354d3db28199667720f0312158d16f13e
SKP <skp.frl@gmail.com>
2019-10-24 22:40:41 +0530

    :sparkles: Add Config component

---
+++ src/components/Config/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ConfigForm } from './ConfigForm';
+
+export const Config: React.FC = () => {
+	return (
+		<div>
+			<h1>Config</h1>
+			<ConfigForm />
+		</div>
+	);
+};
