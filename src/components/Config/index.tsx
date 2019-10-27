df17b73c71149ce3dd261cd55d283602326369e6
SKP <skp.frl@gmail.com>
2019-10-27 16:02:53 +0530

    :sparkles: Implement MiniPresets component

--- src/components/Config/index.tsx
+++ src/components/Config/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { ConfigForm } from './ConfigForm';
+import { Link } from 'react-router-dom';
+import { MiniPresets } from './MiniPresets';
+import {
+	Root,
+	LeftContainer,
+	RightContainer,
+	ConfigTitle,
+	SectionHeading
+} from './styles';
 
 export const Config: React.FC = () => {
 	return (
-		<div>
-			<h1>Config</h1>
-			<ConfigForm />
-		</div>
+		<Root>
+			<LeftContainer>
+				<Link to="/">Home</Link>
+				<ConfigTitle>Config</ConfigTitle>
+			</LeftContainer>
+
+			<RightContainer>
+				<SectionHeading>Select Preset</SectionHeading>
+				<MiniPresets />
+			</RightContainer>
+		</Root>
 	);
 };
