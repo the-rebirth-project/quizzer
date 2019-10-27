c3d058aad48dbdef5b7f2ba4398368d640579f59
SKP <skp.frl@gmail.com>
2019-10-27 16:40:50 +0530

    :bookmark: Release v0.8.4

---
+++ build/precache-manifest.a6f7ff9465fbf198829b585439d42734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912367e0ceb8053805fb4c35c6d1e0d3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f2f79e3ad0c0f1cf0b6d",
+    "url": "./static/js/2.9bce8faa.chunk.js"
+  },
+  {
+    "revision": "655a321b4e18b98523bc",
+    "url": "./static/js/main.c81a1f77.chunk.js"
+  },
+  {
+    "revision": "c75cfd43552c23badf2b",
+    "url": "./static/js/runtime~main.98f4717d.js"
+  },
+  {
+    "revision": "c5805013a25dc7cc7ed5bb33553f1a0f",
+    "url": "./static/media/correctsfx.c5805013.mp3"
+  },
+  {
+    "revision": "16071710b40f9a5f5bc7a36d956af26b",
+    "url": "./static/media/incorrectsfx.16071710.mp3"
+  }
+]);
