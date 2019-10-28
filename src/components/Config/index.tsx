643a19fea998db7bd4dad5129a223af80f0951b8
SKP <skp.frl@gmail.com>
2019-10-28 20:42:26 +0530

    :lipstick: Style Config component

--- src/components/Config/index.tsx
+++ src/components/Config/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTransition } from 'react-spring';
+import { useSelector } from 'react-redux';
 import { MiniPresets } from './MiniPresets';
+import { RootState } from '../../types';
 import {
 	Root,
 	LeftContainer,
@@ -10,17 +13,29 @@ import {
 } from './styles';
 
 export const Config: React.FC = () => {
-	return (
-		<Root>
-			<LeftContainer>
-				<Link to="/">Home</Link>
-				<ConfigTitle>Config</ConfigTitle>
-			</LeftContainer>
+	const { location } = useSelector((state: RootState) => state.router);
+	const shouldTransition = location.pathname.includes('configure') ? 1 : 0;
+	const transitions = useTransition(shouldTransition, p => p, {
+		from: { transform: 'translate(100%,0)' },
+		enter: { transform: 'translate(0%,0)' },
+		leave: { transform: 'translate(-50%,0)' }
+	});
 
-			<RightContainer>
-				<SectionHeading>Select Preset</SectionHeading>
-				<MiniPresets />
-			</RightContainer>
-		</Root>
+	return (
+		<>
+			{transitions.map(({ key, props }) => (
+				<Root key={key} style={props}>
+					<LeftContainer>
+						<Link to="/">Home</Link>
+						<ConfigTitle>Config</ConfigTitle>
+					</LeftContainer>
+
+					<RightContainer>
+						<SectionHeading>Select Preset</SectionHeading>
+						<MiniPresets />
+					</RightContainer>
+				</Root>
+			))}
+		</>
 	);
 };
