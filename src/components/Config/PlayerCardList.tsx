74bdd4af1d065f34d5d7ba6a04160d3968bff9a2
SKP <skp.frl@gmail.com>
2019-10-31 21:55:35 +0530

    :recycle: Refactor PlayerCardList component

---
+++ src/components/Config/PlayerCardList.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { PlayerCard } from './PlayerCard';
+import { RootState } from '../../types';
+import { Root } from './playerCardListStyles';
+
+export const PlayerCardList: React.FC = () => {
+	const numPlayers = [1, 2, 3, 4];
+	const players = useSelector((state: RootState) => state.scoreboard.players);
+	return (
+		<Root>
+			{numPlayers.map((n, i) => (
+				<PlayerCard
+					pNum={n}
+					active={players[i] ? true : false}
+					playerData={players[i]}
+				/>
+			))}
+		</Root>
+	);
+};
