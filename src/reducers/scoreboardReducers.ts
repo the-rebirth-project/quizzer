f877dfd4476b940335b6dbe89371edfae7764f74
SKP <skp.frl@gmail.com>
2019-11-01 21:37:28 +0530

    :sparkles: Add reducers to handle updatePlayerName, addPlayer and removePlayer actions

--- src/reducers/scoreboardReducers.ts
+++ src/reducers/scoreboardReducers.ts
@@ -1,5 +1,10 @@
 import { createReducer } from 'typesafe-actions';
-import { updateScore } from '../actions';
+import {
+	updateScore,
+	updatePlayerName,
+	addPlayer,
+	removePlayer
+} from '../actions';
 import { Player } from '../types';
 
 interface IState {
@@ -8,16 +13,11 @@ interface IState {
 
 // players should ideally be dynamically added when the user is presented with the config component to set up the initial settings of the quiz
 const initialState: IState = {
-	players: [
-		{ id: 0, pName: 'Player 1', score: 0 },
-		{ id: 1, pName: 'Player 2', score: 0 },
-		{ id: 2, pName: 'Player 3', score: 0 }
-	]
+	players: [{ id: 1, pName: 'Player 1', score: 0 }]
 };
 
-export const scoreboardReducer = createReducer(initialState).handleAction(
-	updateScore,
-	(state, action) => ({
+export const scoreboardReducer = createReducer(initialState)
+	.handleAction(updateScore, (state, action) => ({
 		...state,
 		players: state.players.map(p =>
 			p.id === action.payload.id
@@ -28,5 +28,24 @@ export const scoreboardReducer = createReducer(initialState).handleAction(
 				  }
 				: p
 		)
-	})
-);
+	}))
+	.handleAction(updatePlayerName, (state, action) => ({
+		...state,
+		players: state.players.map(p =>
+			p.id === action.payload.id
+				? {
+						id: p.id,
+						pName: action.payload.newName,
+						score: p.score
+				  }
+				: p
+		)
+	}))
+	.handleAction(addPlayer, (state, action) => ({
+		...state,
+		players: [...state.players, action.payload]
+	}))
+	.handleAction(removePlayer, (state, action) => ({
+		...state,
+		players: state.players.filter(p => p.id !== action.payload)
+	}));
