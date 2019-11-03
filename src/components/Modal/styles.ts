083e4a739b2fa92947b968dcadf65e9834961965
SKP <skp.frl@gmail.com>
2019-11-03 19:13:48 +0530

    :lipstick: Add optional prop to modal to specify height and width

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 import { animated } from 'react-spring';
 
+interface ContentProps {
+	width: number | undefined;
+	height: number | undefined;
+}
+
 export const Root = styled.div`
 	display: flex;
 	align-items: center;
@@ -28,14 +33,14 @@ export const Background = styled.div<{ open: boolean }>`
 	transition: all 0.3s;
 `;
 
-export const Content = styled(animated.div)`
+export const Content = styled(animated.div)<ContentProps>`
 	display: flex;
 	flex-direction: column;
 	position: relative;
 	z-index: 200;
 	background-color: ${props => props.theme.colors.secondary};
-	width: 68.3rem;
-	height: 53.7rem;
+	width: ${props => (props.width ? `${props.width}rem` : '68.3rem')};
+	height: ${props => (props.height ? `${props.height}rem` : '53.7rem')};
 	padding: 2rem;
 	border-radius: 20px;
 `;
