73a1cdc9066f335b6c43e82375acc2ebd542c984
SKP <skp.frl@gmail.com>
2019-11-04 15:54:14 +0530

    :sparkles: Add edit preset name feature

--- src/components/Config/PresetItem.tsx
+++ src/components/Config/PresetItem.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, SyntheticEvent } from 'react';
 import { useDispatch } from 'react-redux';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { PresetEditForm } from './PresetEditForm';
 import { setPresetId } from '../../actions';
 import { QuizPreset } from '../../types';
-import { Root, PresetName } from './presetItemStyles';
+import { Root, PresetName, IconContainer, EditIcon } from './presetItemStyles';
 
 interface PresetItemProps extends QuizPreset {
 	selected: boolean;
@@ -10,14 +12,34 @@ interface PresetItemProps extends QuizPreset {
 
 export const PresetItem: React.FC<PresetItemProps> = props => {
 	const dispatch = useDispatch();
+	const [formOpen, setFormOpen] = useState(false);
 
 	const onPresetClick = (): void => {
 		dispatch(setPresetId(props.id));
 	};
 
+	const enableEditForm = (e: SyntheticEvent): void => {
+		e.stopPropagation();
+		setFormOpen(true);
+	};
+
 	return (
 		<Root onClick={onPresetClick} selected={props.selected}>
-			<PresetName>{props.presetName}</PresetName>
+			{formOpen && (
+				<PresetEditForm
+					presetId={props.id}
+					setFormOpen={setFormOpen}
+					initialPresetName={props.presetName}
+				/>
+			)}
+			{!formOpen && (
+				<>
+					<PresetName>{props.presetName}</PresetName>
+					<IconContainer onClick={enableEditForm}>
+						<EditIcon icon={faEdit} selected={props.selected} />
+					</IconContainer>
+				</>
+			)}
 		</Root>
 	);
 };
