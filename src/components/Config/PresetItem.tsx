efd0ead31b0bd31cacda80c657c388d9e0af8ba4
SKP <skp.frl@gmail.com>
2019-11-05 20:52:00 +0530

    :sparkles: Add preset add when selecting last preset

--- src/components/Config/PresetItem.tsx
+++ src/components/Config/PresetItem.tsx
@@ -1,10 +1,17 @@
 import React, { useState, SyntheticEvent } from 'react';
-import { useDispatch } from 'react-redux';
-import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { useDispatch, useSelector } from 'react-redux';
+import uuid from 'uuid/v4';
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { PresetEditForm } from './PresetEditForm';
-import { setPresetId } from '../../actions';
-import { QuizPreset } from '../../types';
-import { Root, PresetName, IconContainer, EditIcon } from './presetItemStyles';
+import { setPresetId, addPreset, removePreset } from '../../actions';
+import { QuizPreset, RootState } from '../../types';
+import {
+	Root,
+	PresetName,
+	IconsContainer,
+	IconContainer,
+	FaIcon
+} from './presetItemStyles';
 
 interface PresetItemProps extends QuizPreset {
 	selected: boolean;
@@ -12,10 +19,36 @@ interface PresetItemProps extends QuizPreset {
 
 export const PresetItem: React.FC<PresetItemProps> = props => {
 	const dispatch = useDispatch();
+	const curPresetId = useSelector((state: RootState) => state.quiz.curPresetId);
+	const presets = useSelector((state: RootState) => state.quiz.presets);
+	const presetData: QuizPreset = {
+		id: props.id,
+		presetName: props.presetName,
+		questions: props.questions
+	};
+	const isLastPreset =
+		presetData.id === presets[presets.length - 1].id ? true : false;
+	const presetIds = presets.map(p => p.id);
 	const [formOpen, setFormOpen] = useState(false);
 
+	// bug when deleting preset when only one present
+
+	const handlePresetAdd = (shouldSetId?: boolean): void => {
+		const newPreset: QuizPreset = {
+			id: uuid(),
+			presetName: 'New Preset',
+			questions: []
+		};
+		dispatch(addPreset(newPreset));
+		shouldSetId && dispatch(setPresetId(newPreset.id));
+	};
+
 	const onPresetClick = (): void => {
-		dispatch(setPresetId(props.id));
+		if (!formOpen) {
+			if (props.id !== curPresetId) dispatch(setPresetId(props.id));
+			// if presetData of the component is last in the array of presets, then add a new blank preset
+			if (isLastPreset) handlePresetAdd();
+		}
 	};
 
 	const enableEditForm = (e: SyntheticEvent): void => {
