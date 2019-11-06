494808bb159d93263bbf942f42b4cdc27eef7088
SKP <skp.frl@gmail.com>
2019-11-06 21:01:45 +0530

    :sparkles: Add reusable Layout component

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import { animated } from 'react-spring';
+import { Title } from '../MainMenu/styles';
+
+export const Root = styled(animated.div)`
+	display: flex;
+	position: relative;
+	z-index: 1000;
+	height: 100vh;
+	width: 100vw;
+`;
+
+export const LeftContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	width: 30vw;
+	height: 100vh;
+	position: relative;
+	z-index: 1000;
+`;
+
+// RightContainer will contain the respective scores
+export const RightContainer = styled(animated.div)`
+	::-webkit-scrollbar {
+		width: 0 !important;
+	}
+
+	display: flex;
+	width: 70vw;
+	height: 100vh;
+	align-items: flex-start;
+	flex-direction: column;
+	overflow-y: scroll;
+	position: relative;
+`;
+
+export const MainTitle = styled(Title)`
+	letter-spacing: 0.9rem;
+	font-size: 8rem;
+`;
+
+export const SectionHeading = styled.h2`
+	text-transform: uppercase;
+	color: ${props => props.theme.colors.grey};
+	font-size: 7rem;
+	font-weight: 300;
+	margin-bottom: 2.3rem;
+	padding-bottom: 0.5rem;
+	letter-spacing: 2rem;
+	text-align: center;
+`;
+
+export const NavBtnContainer = styled.nav`
+	align-self: flex-end;
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	padding: 0rem 1rem;
+	align-items: center;
+`;
