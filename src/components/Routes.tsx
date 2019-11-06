a215726f2be364d45277bece2a3b46f0c57b15a8
SKP <skp.frl@gmail.com>
2019-11-06 21:03:02 +0530

    :mag: Add routing for Log component

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Menu } from './MainMenu';
-import { Question } from './Question';
-import { CreateQuiz } from './CreateQuiz';
-import { history } from '../store';
 import { Switch, Route } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import { useSelector } from 'react-redux';
-import { RootState } from '../types';
+import { Menu } from './MainMenu';
+import { Question } from './Question';
+import { CreateQuiz } from './CreateQuiz';
 import { Config } from './Config';
+import { Log } from './Log';
+import { history } from '../store';
+import { RootState } from '../types';
 
 export const Routes: React.FC = () => {
 	const started = useSelector((state: RootState) => state.quiz.started);
@@ -28,6 +29,7 @@ export const Routes: React.FC = () => {
 					path="/configure/:pageNum"
 					render={routeProps => <Config {...routeProps} />}
 				/>
+				<Route exact path="/log" render={() => <Log />} />
 			</Switch>
 		</ConnectedRouter>
 	);
