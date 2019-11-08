d3f661c0eb9fc1389f4ab884addbaa6db4a5308d
SKP <skp.frl@gmail.com>
2019-11-08 21:44:42 +0530

    :bug: Fix type definitions for action param

--- src/actions/logActions.ts
+++ src/actions/logActions.ts
@@ -2,6 +2,7 @@ import { createStandardAction } from 'typesafe-actions';
 import { Question, LOG_USER_CHOICE } from '../types';
 
 interface logUserChoicePayload extends Question {
+	qNum: number;
 	userChoice: string;
 }
 
