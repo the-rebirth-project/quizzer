b6347a258113c11a278660f11cb72f751b8ba880
SKP <skp.frl@gmail.com>
2019-11-09 19:53:37 +0530

    :sparkles: Add calculatedScore data to action payload

--- src/actions/logActions.ts
+++ src/actions/logActions.ts
@@ -4,6 +4,7 @@ import { Question, LOG_USER_CHOICE } from '../types';
 interface logUserChoicePayload extends Question {
 	qNum: number;
 	userChoice: string;
+	calculatedScore: number;
 }
 
 export const logUserChoice = createStandardAction(LOG_USER_CHOICE)<
