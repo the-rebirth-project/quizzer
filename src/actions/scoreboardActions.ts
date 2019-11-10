b8849c683249fec10b9c357fedb64186bb34983f
SKP <skp.frl@gmail.com>
2019-11-10 17:57:43 +0530

    :sparkles: Add resetScore action

--- src/actions/scoreboardActions.ts
+++ src/actions/scoreboardActions.ts
@@ -4,7 +4,8 @@ import {
 	UPDATE_SCORE,
 	UPDATE_PLAYER_NAME,
 	ADD_PLAYER,
-	REMOVE_PLAYER
+	REMOVE_PLAYER,
+	RESET_SCORE
 } from '../types';
 
 export const updateScore = createStandardAction(UPDATE_SCORE)<{
@@ -20,3 +21,4 @@ export const updatePlayerName = createStandardAction(UPDATE_PLAYER_NAME)<{
 export const addPlayer = createStandardAction(ADD_PLAYER)<Player>();
 // uses player id to figure out which player to remove
 export const removePlayer = createStandardAction(REMOVE_PLAYER)<number>();
+export const resetScore = createStandardAction(RESET_SCORE)<undefined>();
