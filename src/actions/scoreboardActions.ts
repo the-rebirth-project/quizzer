87d40172a4e9b6180887cca0294e20c09258557c
SKP <skp.frl@gmail.com>
2019-11-14 20:21:54 +0530

    :sparkles: Add changePlayerName and setPlayerRanking actions

--- src/actions/scoreboardActions.ts
+++ src/actions/scoreboardActions.ts
@@ -1,24 +1,33 @@
 import { createStandardAction } from 'typesafe-actions';
 import {
-	Player,
-	UPDATE_SCORE,
-	UPDATE_PLAYER_NAME,
-	ADD_PLAYER,
-	REMOVE_PLAYER,
-	RESET_SCORE
+  Player,
+  UPDATE_SCORE,
+  UPDATE_PLAYER_NAME,
+  ADD_PLAYER,
+  REMOVE_PLAYER,
+  CHANGE_PLAYER_NAME,
+  RESET_SCORE,
+  SET_PLAYER_RANKING
 } from '../types';
 
 export const updateScore = createStandardAction(UPDATE_SCORE)<{
-	score: number;
-	id: number;
+  score: number;
+  id: number;
 }>();
 
 export const updatePlayerName = createStandardAction(UPDATE_PLAYER_NAME)<{
-	id: number;
-	newName: string;
+  id: number;
+  newName: string;
 }>();
 
 export const addPlayer = createStandardAction(ADD_PLAYER)<Player>();
 // uses player id to figure out which player to remove
 export const removePlayer = createStandardAction(REMOVE_PLAYER)<number>();
 export const resetScore = createStandardAction(RESET_SCORE)<undefined>();
+export const changePlayerName = createStandardAction(CHANGE_PLAYER_NAME)<{
+  id: number;
+  newName: string;
+}>();
+export const setPlayerRanking = createStandardAction(SET_PLAYER_RANKING)<
+  Player[]
+>();
