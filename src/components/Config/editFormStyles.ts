fa015ef3709a04003af2028bb8a2e91ac6c3650d
SKP <skp.frl@gmail.com>
2019-11-14 20:26:53 +0530

    :lipstick: Style EditForm

---
+++ src/components/Config/editFormStyles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { StyledInputField } from '../CreateQuiz/formStyles';
+
+export const EditField = styled(StyledInputField)`
+	font-size: 3rem;
+	border-radius: 5px;
+	text-transform: uppercase;
+	font-weight: 300;
+	margin-left: -0.8rem;
+	flex: 0 0 70%;
+	width: 100%;
+	background-color: transparent;
+`;
