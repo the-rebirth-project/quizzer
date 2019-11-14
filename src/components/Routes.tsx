45aa0e65dca0daec951effcff44d2ee453e21e1b
SKP <skp.frl@gmail.com>
2019-11-14 20:28:54 +0530

    :sparkles: Implement routing for WinOverlay

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -7,39 +7,41 @@ import { Question } from './Question';
 import { CreateQuiz } from './CreateQuiz';
 import { Config } from './Config';
 import { TurnOverlay } from './TurnOverlay';
+import { WinOverlay } from './WinOverlay';
 import { Log } from './Log';
 import { history } from '../store';
 import { RootState } from '../types';
 
 export const Routes: React.FC = () => {
-	const started = useSelector((state: RootState) => state.quiz.started);
+  const started = useSelector((state: RootState) => state.quiz.started);
 
-	return (
-		<ConnectedRouter history={history}>
-			<Switch>
-				<Route exact path="/" render={() => <Menu />} />
-				<Route exact path="/log" render={() => <Log />} />
-				{started && (
-					<>
-						<Route
-							exact
-							path="/start/q/:qId"
-							render={routeProps => <Question {...routeProps} />}
-						/>
-						<Route
-							exact
-							path="/playerturn/:playerId/:nextQuestionNum"
-							render={routeProps => <TurnOverlay {...routeProps} />}
-						/>
-					</>
-				)}
-				<Route exact path="/create" render={() => <CreateQuiz />} />
-				<Route
-					exact
-					path="/configure/:pageNum"
-					render={routeProps => <Config {...routeProps} />}
-				/>
-			</Switch>
-		</ConnectedRouter>
-	);
+  return (
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path="/" render={() => <Menu />} />
+        <Route exact path="/log" render={() => <Log />} />
+        {started && (
+          <>
+            <Route
+              exact
+              path="/start/q/:qId"
+              render={routeProps => <Question {...routeProps} />}
+            />
+            <Route exact path="/finalresults" render={() => <WinOverlay />} />
+            <Route
+              exact
+              path="/playerturn/:playerId/:nextQuestionNum"
+              render={routeProps => <TurnOverlay {...routeProps} />}
+            />
+          </>
+        )}
+        <Route exact path="/create" render={() => <CreateQuiz />} />
+        <Route
+          exact
+          path="/configure/:pageNum"
+          render={routeProps => <Config {...routeProps} />}
+        />
+      </Switch>
+    </ConnectedRouter>
+  );
 };
