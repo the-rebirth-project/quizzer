9696ec17c0c9a9d82a99820d43b24eaaf3438cbb
SKP <skp.frl@gmail.com>
2019-11-14 20:23:06 +0530

    :sparkles: Implement PlayerCardEditForm component

--- src/components/Config/PlayerCard.tsx
+++ src/components/Config/PlayerCard.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { PlayerCardEditForm } from './PlayerCardEditForm';
 import { addPlayer, removePlayer } from '../../actions';
 import { Player } from '../../types';
-import { Root, PlayerName } from './playerCardStyles';
+import { Root, PlayerName, IconContainer, FaIcon } from './playerCardStyles';
 
 interface PlayerCardProps {
 	pNum: number;
@@ -16,15 +18,42 @@ export const PlayerCard: React.FC<PlayerCardProps> = ({
 	playerData
 }) => {
 	const dispatch = useDispatch();
+	const [formOpen, setFormOpen] = useState(false);
+
+	const enableEditForm = (e: React.SyntheticEvent): void => {
+		e.stopPropagation();
+		setFormOpen(true);
+	};
+
 	const onCardClick = (): void => {
 		// push new player else if already selected, pop new player off of players array
-		playerData
+		!formOpen && playerData
 			? dispatch(removePlayer(playerData.id))
 			: dispatch(addPlayer({ id: pNum, pName: `Player ${pNum}`, score: 0 }));
 	};
+
 	return (
 		<Root active={active} onClick={onCardClick}>
-			<PlayerName>Player {pNum}</PlayerName>
+			{!formOpen && (
+				<>
+					{/* Only show the edit icon if the player card is selected */}
+					{playerData && (
+						<IconContainer onClick={enableEditForm}>
+							<FaIcon icon={faEdit} />
+						</IconContainer>
+					)}
+					<PlayerName>
+						{playerData ? playerData.pName : `Player ${pNum}`}
+					</PlayerName>
+				</>
+			)}
+			{formOpen && (
+				<PlayerCardEditForm
+					setFormOpen={setFormOpen}
+					playerId={pNum}
+					initialPlayerName={`Player ${pNum}`}
+				/>
+			)}
 		</Root>
 	);
 };
