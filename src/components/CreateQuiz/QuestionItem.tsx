d4bc3ad2c544a7f6f4a4adcd206bfca2b8aacf2e
SKP <skp.frl@gmail.com>
2019-11-17 19:31:44 +0530

    :lipstick: Refactor QuestionItem styles

--- src/components/CreateQuiz/QuestionItem.tsx
+++ src/components/CreateQuiz/QuestionItem.tsx
@@ -6,57 +6,72 @@ import { Modal } from '../Modal';
 import { showModal, deleteQuestion } from '../../actions';
 import { EditForm } from './EditForm';
 import { Question, RootState } from '../../types';
-import { Root, QuestionContainer, QuestionNum } from './questionItemStyles';
+import {
+  Root,
+  QuestionContainer,
+  QuestionNum,
+  MetadataContainer,
+  Metadata,
+  QuestionTextContainer
+} from './questionItemStyles';
 
 interface QuestionItemProps {
-	question: Question;
-	type: string;
-	qPos: number;
+  question: Question;
+  type: string;
+  qPos: number;
 }
 
 const WrappedComponent: React.FC<QuestionItemProps> = ({
-	type,
-	question,
-	qPos
+  type,
+  question,
+  qPos
 }) => {
-	const dispatch = useDispatch();
-	const toolbarSt = useSelector((state: RootState) => state.toolbar);
-	const { editModeState, deleteModeState } = toolbarSt;
-	// have to use component state so that all of the modals don't open at once
-	const [modalOpen, setModalOpen] = useState(false);
+  const dispatch = useDispatch();
+  const toolbarSt = useSelector((state: RootState) => state.toolbar);
+  const { editModeState, deleteModeState } = toolbarSt;
+  // have to use component state so that all of the modals don't open at once
+  const [modalOpen, setModalOpen] = useState(false);
 
-	const onQuestionClick = (): void => {
-		// only open modal if editMode is set to true
-		switch (true) {
-			case editModeState:
-				dispatch(showModal());
-				setModalOpen(true);
-				break;
-			case deleteModeState:
-				dispatch(deleteQuestion(question.qId));
-				break;
-			default:
-				setModalOpen(false);
-		}
-	};
+  const onQuestionClick = (): void => {
+    // only open modal if editMode is set to true
+    switch (true) {
+      case editModeState:
+        dispatch(showModal());
+        setModalOpen(true);
+        break;
+      case deleteModeState:
+        dispatch(deleteQuestion(question.qId));
+        break;
+      default:
+        setModalOpen(false);
+    }
+  };
 
-	return (
-		<Root>
-			<QuestionContainer onClick={onQuestionClick}>
-				<QuestionNum>{qPos}. </QuestionNum>
-				{question.question}
-			</QuestionContainer>
-			<Modal
-				open={modalOpen}
-				setModalOpen={setModalOpen}
-				key={uuid()}
-				aria-labelledby="modal-label"
-				aria-describedby="model-desc"
-			>
-				<EditForm question={question} setModalOpen={setModalOpen} />
-			</Modal>
-		</Root>
-	);
+  return (
+    <Root>
+      <QuestionContainer onClick={onQuestionClick}>
+        <QuestionTextContainer>
+          <QuestionNum>{qPos}. </QuestionNum>
+          {question.question}
+        </QuestionTextContainer>
+        {question.player && (
+          <MetadataContainer>
+            <Metadata>Assigned Player: {question.player.pName}</Metadata>
+            <Metadata>Difficulty: {question.difficulty}</Metadata>
+          </MetadataContainer>
+        )}
+      </QuestionContainer>
+      <Modal
+        open={modalOpen}
+        setModalOpen={setModalOpen}
+        key={uuid()}
+        aria-labelledby="modal-label"
+        aria-describedby="model-desc"
+      >
+        <EditForm question={question} setModalOpen={setModalOpen} />
+      </Modal>
+    </Root>
+  );
 };
 
 // exports the above declared functional component as a child of SortableElement
