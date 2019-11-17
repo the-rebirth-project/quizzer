3cb4a3ea309498509b9d86fcc86dc8d106b3ac8a
SKP <skp.frl@gmail.com>
2019-11-17 19:41:53 +0530

    :lipstick: Clean up hover transition for edit tool on PlayerCard

--- src/components/Config/playerCardStyles.ts
+++ src/components/Config/playerCardStyles.ts
@@ -1,52 +1,79 @@
 import styled, { css } from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface RootProps {
-	active: boolean;
+  active: boolean;
 }
 
 export const Root = styled.div<RootProps>`
-	cursor: pointer;
-	width: 20.6rem;
-	height: 26.3rem;
-	background-image: linear-gradient(
-		120deg,
-		${props => props.theme.colors.teal},
-		${props => props.theme.colors.amethyst}
-	);
-	${props =>
-		props.active
-			? css`
-					background-size: 400%;
-					background-color: transparent;
-					box-shadow: 0rem 0.3rem 0.6rem rgba(26, 130, 330, 0.25);
-			  `
-			: css`
-					background-size: 5000%;
-					background-color: ${props => props.theme.colors.grey};
-					box-shadow: 0rem 0.3rem 0.6rem rgba(var(--teal-rgb-color), 0.25);
-			  `}
-	border-radius: 1rem;
-	transition-property: background-size, background-color, box-shadow, transform;
-	transition-duration: 0.7s, 0.7s, 0.7s, 0.3s;
-	transition-timing-function: ease-in-out;
+  position: relative;
+  cursor: pointer;
+  width: 20.6rem;
+  height: 26.3rem;
+  background-image: linear-gradient(
+    120deg,
+    ${props => props.theme.colors.teal},
+    ${props => props.theme.colors.amethyst}
+  );
+  ${props =>
+    props.active
+      ? css`
+          background-size: 400%;
+          background-color: transparent;
+          box-shadow: 0rem 0.3rem 0.6rem rgba(26, 130, 330, 0.25);
+        `
+      : css`
+          background-size: 5000%;
+          background-color: ${props => props.theme.colors.grey};
+          box-shadow: 0rem 0.3rem 0.6rem rgba(var(--teal-rgb-color), 0.25);
+        `}
+  border-radius: 1rem;
+  transition-property: background-size, background-color, box-shadow, transform;
+  transition-duration: 0.7s, 0.7s, 0.7s, 0.3s;
+  transition-timing-function: ease-in-out;
 
-	:hover {
-		transform: translateY(-3px);
-	}
+  :hover {
+    transform: translateY(-3px);
+  }
 
-	:active {
-		transform: translateY(0px);
-	}
+  :active {
+    transform: translateY(0px);
+  }
 
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	color: ${props => props.theme.colors.secondary};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${props => props.theme.colors.secondary};
 `;
 
 export const PlayerName = styled.span`
-	font-size: 2.5rem;
-	letter-spacing: 0.3rem;
-	font-weight: 400;
-	text-transform: uppercase;
+  font-size: 3rem;
+  letter-spacing: 0.3rem;
+  font-weight: 300;
+  text-transform: uppercase;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 90%;
+  text-align: center;
+`;
+
+export const IconContainer = styled.div`
+  height: 3rem;
+  width: 3rem;
+
+  position: absolute;
+  top: 2%;
+  right: 6%;
+`;
+
+export const FaIcon = styled(FontAwesomeIcon)`
+  font-size: 3rem;
+  color: ${props => props.theme.colors.secondary};
+  transform: scale(0.9);
+  transition: all 0.2s ease-in-out;
+
+  :hover {
+    transform: scale(1);
+  }
 `;
