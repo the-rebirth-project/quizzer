353a1375c515dc35e1126c857ce363d13d715c32
SKP <skp.frl@gmail.com>
2019-11-17 19:41:14 +0530

    :bug: Fix user choice not being logged properly

--- src/reducers/logReducers.ts
+++ src/reducers/logReducers.ts
@@ -3,38 +3,38 @@ import { logUserChoice, clearLog } from '../actions';
 import { QuestionLog } from '../types';
 
 interface IState {
-	readonly sessionLog: QuestionLog[];
+  readonly sessionLog: QuestionLog[];
 }
 
 const initialState: IState = {
-	sessionLog: []
+  sessionLog: []
 };
 
 export const logReducer = createReducer(initialState)
-	.handleAction(logUserChoice, (state, action) => {
-		const {
-			qId,
-			question,
-			userChoice,
-			correct_answer,
-			qNum,
-			calculatedScore
-		} = action.payload;
-		const questionLog: QuestionLog = {
-			qNum,
-			qId,
-			question,
-			userChoice,
-			choiceValid: userChoice === correct_answer ? true : false,
-			correctAnswer: correct_answer,
-			calculatedScore
-		};
-		return {
-			...state,
-			sessionLog: [...state.sessionLog, questionLog]
-		};
-	})
-	.handleAction(clearLog, (state, _) => ({
-		...state,
-		sessionLog: []
-	}));
+  .handleAction(logUserChoice, (state, action) => {
+    const {
+      qId,
+      question,
+      userChoice,
+      correct_answer,
+      qNum,
+      calculatedScore
+    } = action.payload;
+    const questionLog: QuestionLog = {
+      qNum,
+      qId,
+      question,
+      userChoice,
+      choiceValid: userChoice === correct_answer ? true : false,
+      correctAnswer: correct_answer,
+      calculatedScore
+    };
+    return {
+      ...state,
+      sessionLog: [...state.sessionLog, questionLog]
+    };
+  })
+  .handleAction(clearLog, (state, _) => ({
+    ...state,
+    sessionLog: []
+  }));
