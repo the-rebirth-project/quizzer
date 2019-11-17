945f1c73e539e53f727c20f6d3377dcbd35b9cda
SKP <skp.frl@gmail.com>
2019-11-17 19:37:11 +0530

    :fire: Remove unnecessary animation transition

--- src/components/CreateQuiz/index.tsx
+++ src/components/CreateQuiz/index.tsx
@@ -1,70 +1,60 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { animated, useTransition } from 'react-spring';
+import { useNavigation } from 'react-navi';
 import { arrayMove } from '../../helpers';
 import { SortEnd } from 'react-sortable-hoc';
 import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
-import { push } from 'connected-react-router';
 import { MiniQuestions } from './MiniQuestions';
 import { Toolbar } from './Toolbar';
 import { sortQuestion } from '../../actions/createQuizActions';
 import { RootState } from '../../types';
 import { NavBtnsContainer, ButtonContainer, NavBtn } from '../Layout/styles';
 import {
-	GreyBG,
-	GlobalStyle,
-	Root,
-	Sidebar,
-	CreateTitle,
-	MiniQuestionsContainer
+  GreyBG,
+  GlobalStyle,
+  Root,
+  Sidebar,
+  CreateTitle,
+  MiniQuestionsContainer
 } from './styles';
 
 export const CreateQuiz: React.FC = () => {
-	const dispatch = useDispatch();
-	const questions = useSelector((state: RootState) => state.quiz.questions);
-	const { location } = useSelector((state: RootState) => state.router);
-	const shouldTransition = location.pathname.includes('create') ? 1 : 0;
-	const transitions = useTransition(shouldTransition, p => p, {
-		from: { transform: 'translate(100%,0)' },
-		enter: { transform: 'translate(0%,0)' },
-		leave: { transform: 'translate(-50%,0)' }
-	});
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const questions = useSelector((state: RootState) => state.quiz.questions);
 
-	const handleOnSortEnd = ({ oldIndex, newIndex }: SortEnd): void => {
-		const sortedQuestions = arrayMove(oldIndex, newIndex, questions);
-		dispatch(sortQuestion(sortedQuestions));
-	};
+  const handleOnSortEnd = ({ oldIndex, newIndex }: SortEnd): void => {
+    const sortedQuestions = arrayMove(oldIndex, newIndex, questions);
+    dispatch(sortQuestion(sortedQuestions));
+  };
 
-	const goLeft = (): void => {
-		dispatch(push('/'));
-	};
+  const goLeft = (): void => {
+    navigation.navigate('/');
+  };
 
-	return (
-		<GreyBG>
-			<GlobalStyle />
-			{transitions.map(({ props, key }) => (
-				<animated.div key={key} style={props}>
-					<Root>
-						<Sidebar>
-							<Toolbar />
-							<CreateTitle>Create</CreateTitle>
-							<NavBtnsContainer>
-								<ButtonContainer onClick={goLeft} left>
-									<NavBtn icon={faLongArrowAltLeft} />
-								</ButtonContainer>
-							</NavBtnsContainer>
-						</Sidebar>
-						<MiniQuestionsContainer>
-							<MiniQuestions
-								distance={3}
-								axis="y"
-								lockAxis="y"
-								onSortEnd={handleOnSortEnd}
-							/>
-						</MiniQuestionsContainer>
-					</Root>
-				</animated.div>
-			))}
-		</GreyBG>
-	);
+  return (
+    <GreyBG>
+      <GlobalStyle />
+
+      <Root>
+        <Sidebar>
+          <Toolbar />
+          <CreateTitle>Create</CreateTitle>
+          <NavBtnsContainer>
+            <ButtonContainer onClick={goLeft} left>
+              <NavBtn icon={faLongArrowAltLeft} />
+            </ButtonContainer>
+          </NavBtnsContainer>
+        </Sidebar>
+        <MiniQuestionsContainer>
+          <MiniQuestions
+            distance={3}
+            axis="y"
+            lockAxis="y"
+            onSortEnd={handleOnSortEnd}
+          />
+        </MiniQuestionsContainer>
+      </Root>
+    </GreyBG>
+  );
 };
