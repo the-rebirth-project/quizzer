3821a5213ecd1900d6312c38b92cf37df9c9cbf1
SKP <skp.frl@gmail.com>
2019-11-21 22:16:31 +0530

    :bug: Fix being able to select 0 players

--- src/components/Config/PlayerCardList.tsx
+++ src/components/Config/PlayerCardList.tsx
@@ -5,18 +5,18 @@ import { RootState } from '../../types';
 import { Root } from './playerCardListStyles';
 
 export const PlayerCardList: React.FC = () => {
-	const numPlayers = [1, 2, 3, 4];
-	const players = useSelector((state: RootState) => state.scoreboard.players);
-	return (
-		<Root>
-			{numPlayers.map(n => (
-				<PlayerCard
-					key={n}
-					pNum={n}
-					active={players.filter(p => p.id === n)[0] ? true : false}
-					playerData={players.filter(p => p.id === n)[0]}
-				/>
-			))}
-		</Root>
-	);
+  const numPlayers = [1, 2, 3, 4];
+  const players = useSelector((state: RootState) => state.scoreboard.players);
+  return (
+    <Root>
+      {numPlayers.map(n => (
+        <PlayerCard
+          key={n}
+          pNum={n}
+          active={players.filter(p => p.id === n)[0] ? true : false}
+          playerData={players.filter(p => p.id === n)[0]}
+        />
+      ))}
+    </Root>
+  );
 };
