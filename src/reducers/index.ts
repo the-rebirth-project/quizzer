54808646a89710e5425615e8e8d5a3bf933733b5
SKP <skp.frl@gmail.com>
2019-11-21 22:08:41 +0530

    :sparkles: Add snackbarReducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -6,14 +6,16 @@ import { modalReducer } from './modalReducers';
 import { toolbarReducer } from './toolbarReducers';
 import { scoreboardReducer } from './scoreboardReducers';
 import { logReducer } from './logReducers';
+import { snackbarReducer } from './snackbarReducers';
 
 export const rootReducer = (history: any) =>
-	combineReducers({
-		router: connectRouter(history),
-		quiz: quizReducer,
-		question: questionReducer,
-		modal: modalReducer,
-		toolbar: toolbarReducer,
-		scoreboard: scoreboardReducer,
-		log: logReducer
-	});
+  combineReducers({
+    router: connectRouter(history),
+    quiz: quizReducer,
+    question: questionReducer,
+    modal: modalReducer,
+    toolbar: toolbarReducer,
+    scoreboard: scoreboardReducer,
+    log: logReducer,
+    snackbar: snackbarReducer
+  });
