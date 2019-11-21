52b95f8bc29ed5b38e129d5fe09cd063403489d5
SKP <skp.frl@gmail.com>
2019-11-21 22:20:41 +0530

    :sparkles: Implement SnackbarValidator in CreateQuiz component

--- src/components/CreateQuiz/index.tsx
+++ src/components/CreateQuiz/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigation } from 'react-navi';
 import { arrayMove } from '../../helpers';
 import { SortEnd } from 'react-sortable-hoc';
+import { push } from 'connected-react-router';
 import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
 import { MiniQuestions } from './MiniQuestions';
+import { SnackbarValidator } from '../SnackbarValidator';
 import { Toolbar } from './Toolbar';
 import { sortQuestion } from '../../actions/createQuizActions';
+import { assignPlayerToQuestion, savePreset, showSnackbar } from '../../actions';
 import { RootState } from '../../types';
 import { NavBtnsContainer, ButtonContainer, NavBtn } from '../Layout/styles';
 import {
@@ -20,16 +22,36 @@ import {
 
 export const CreateQuiz: React.FC = () => {
   const dispatch = useDispatch();
-  const navigation = useNavigation();
+  const failedValidator = useSelector((state: RootState) => state.snackbar.failedValidator);
   const questions = useSelector((state: RootState) => state.quiz.questions);
+  const players = useSelector((state: RootState) => state.scoreboard.players);
+  const curPresetId = useSelector((state: RootState) => state.quiz.curPresetId);
+  const curPresetData = useSelector(
+    (state: RootState) => state.quiz.presets
+  ).filter(p => p.id === curPresetId)[0];
 
   const handleOnSortEnd = ({ oldIndex, newIndex }: SortEnd): void => {
     const sortedQuestions = arrayMove(oldIndex, newIndex, questions);
     dispatch(sortQuestion(sortedQuestions));
+    players.length > 1 && dispatch(assignPlayerToQuestion(players));
   };
 
   const goLeft = (): void => {
-    navigation.navigate('/');
+    const saveAndGoToMenu = (): void => {
+      dispatch(push('/'));
+      const newPresetData = {
+      ...curPresetData,
+      questions
+    };
+    dispatch(
+      savePreset({
+        id: curPresetId,
+        newPresetData
+      })
+      );
+    }
+
+    failedValidator ? dispatch(showSnackbar()) : saveAndGoToMenu();
   };
 
   return (
@@ -54,6 +76,8 @@ export const CreateQuiz: React.FC = () => {
             onSortEnd={handleOnSortEnd}
           />
         </MiniQuestionsContainer>
+
+        <SnackbarValidator validators={[{validateCondition: questions.length % players.length === 0, errorMessage: `Preset must have equal number of questions for each player`}]}/>
       </Root>
     </GreyBG>
   );
