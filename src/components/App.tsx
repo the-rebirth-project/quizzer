ce6dd1c943ac4e6321222042af529bfdb691bb66
SKP <skp.frl@gmail.com>
2019-11-21 22:12:58 +0530

    :dizzy: Add page transitions

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import { animated, useSpring, useTransition } from 'react-spring';
-import { useViewElement, useCurrentRoute } from 'react-navi';
+import { animated, useSpring } from 'react-spring';
 import { useDispatch, useSelector } from 'react-redux';
+import { Routes } from './Routes';
 import { setPresetId } from '../actions';
 import { RootState } from '../types';
 
@@ -17,51 +17,15 @@ const Background = styled(animated.div)`
 
 export const App: React.FC = () => {
   const dispatch = useDispatch();
-  const route = useCurrentRoute();
   // using both location.pathname and the started boolean in order to determine if we should flip the background. maybe there's a better solution?
-  const location = route.url;
+  const { location } = useSelector((state: RootState) => state.router);
   const started = useSelector((state: RootState) => state.quiz.started);
   const quizPresets = useSelector((state: RootState) => state.quiz.presets);
-  const questions = useSelector((state: RootState) => state.quiz.questions);
-  const players = useSelector((state: RootState) => state.scoreboard.players);
-
-  const viewElement = useViewElement();
-  const transitions = useTransition(viewElement, route.url.href, {
-    from: { transform: 'translate3d(100%,0,0)' },
-    enter: { transform: 'translate3d(0%,0,0)' },
-    leave: { opacity: 0, transform: 'translate3d(-100%,0,0)' }
-  });
 
   useEffect(() => {
     quizPresets[0] && dispatch(setPresetId(quizPresets[0].id));
     // eslint-disable-next-line
   }, []);
-
-  useEffect(() => {
-    questions.map((q, i) => {
-      // NOTE: THIS CODE BREAKS IF NUMBER OF PLAYERS EXCEEDS 4.
-      const prevQuestion = questions[i - 1];
-      let playerI: number = 0;
-      if (prevQuestion) playerI = players.indexOf(questions[i - 1].player);
-      const numOfIndices = players.length - 1;
-      // we're brute handling all edge cases here. maybe try implementing a better solution?
-      if (!prevQuestion) {
-        q.player = players[0];
-      } else if (numOfIndices - playerI === 1) {
-        q.player = players[numOfIndices];
-      } else if (playerI === 0) {
-        q.player = players[playerI + 1];
-      } else {
-        q.player = players[numOfIndices - playerI];
-      }
-
-      if (players.length === 1) q.player = players[0];
-
-      return {
-        ...q
-      };
-    });
-  }, [dispatch, questions, players]);
 
   useEffect(() => {
     window.localStorage.setItem('quizPresets', JSON.stringify(quizPresets));
@@ -97,19 +61,7 @@ export const App: React.FC = () => {
 
   return (
     <Background style={animProps}>
-      {transitions.map(({ item, key, props }) => (
-        <animated.div
-          key={key}
-          style={{
-            ...props,
-            position: 'absolute',
-            top: 0,
-            width: '100%'
-          }}
-        >
-          {item}
-        </animated.div>
-      ))}
+      <Routes />
     </Background>
   );
 };
